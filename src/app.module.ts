2b1be08ae3f32c3d3fc68b44fd0822c34329eb01
unknown <raquelresende_@outlook.com>
2022-10-17 09:34:03 -0300

    pasta categoria

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoriaModule } from './categoria/categoria.module';
 
 @Module({
-  imports: [],
+  imports: [CategoriaModule],
   controllers: [AppController],
   providers: [AppService],
 })
