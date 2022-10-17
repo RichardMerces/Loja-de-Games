dce81ec8a11ac1b79c9a4b15b5d7b150fe829714
rejanequeiroz <rejanequeiroz24@gmail.com>
2022-10-17 11:50:53 -0300

    modificando produtos.controller.ts

--- src/produtos/produtos.controller.ts
+++ src/produtos/produtos.controller.ts
@@ -1,34 +1,46 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode } from '@nestjs/common';
 import { ProdutosService } from './produtos.service';
 import { CreateProdutoDto } from './dto/create-produto.dto';
 import { UpdateProdutoDto } from './dto/update-produto.dto';
+import { get } from 'http';
+import { Produto } from './entities/produto.entity';
+
 
 @Controller('produtos')
 export class ProdutosController {
   constructor(private readonly produtosService: ProdutosService) {}
 
+  
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  findAll(): Promise<Produto[]> {
+    return this.produtosService.findAll();
+  }
+  
+  @Get(':id')
+  @HttpCode(HttpStatus.OK)
+  findOne(@Param('id') id: string) {
+    return this.produtosService.findOne(id);
+  }
+  
+  @Get('/titulo')
+  @HttpCode(HttpStatus.OK)
+  findByName(@Param('nome') nome: string): Promise<Produto[]>{
+    return this.produtosService.findByName(nome);
+  }
+  
   @Post()
   create(@Body() createProdutoDto: CreateProdutoDto) {
     return this.produtosService.create(createProdutoDto);
   }
-
-  @Get()
-  findAll() {
-    return this.produtosService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.produtosService.findOne(+id);
-  }
-
+  
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateProdutoDto: UpdateProdutoDto) {
-    return this.produtosService.update(+id, updateProdutoDto);
+    return this.produtosService.update(id, updateProdutoDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.produtosService.remove(+id);
+    return this.produtosService.remove(id);
   }
 }
