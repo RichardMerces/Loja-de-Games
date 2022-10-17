8b46b69fff178f565c7b7144674ef1d8fa9bda29
mussersupremo <davigmusser@gmail.com>
2022-10-17 13:11:55 -0300

    finalemte foi

--- src/produtos/entities/produto.entity.ts
+++ src/produtos/entities/produto.entity.ts
@@ -1,1 +1,17 @@
-export class Produto {}
+import {Entity, Column, PrimaryGeneratedColumn} from "typeorm"
+
+@Entity()
+export class Produto 
+{
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column({length: 100})
+    name: string
+
+    @Column({length: 100})
+    price: number
+
+    @Column({length: 100})
+    type: string
+}
